241dd97eeda5331a59cb24a651fa62822fcac2e8
barak <balmeshaan@gmail.com>
2019-10-05 22:33:36 +0300

    task 4 first commit

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+	name = models.CharField(max_length=105)
+	description = models.TextField()
+	opening_time = models.CharField(max_length=105)
+	closing_time = models.CharField(max_length=105)
